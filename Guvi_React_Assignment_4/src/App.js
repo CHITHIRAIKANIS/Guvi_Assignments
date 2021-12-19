aefe45c4f3b76a46671afcc68ef6e95067a995c3
CHITHIRAIKANI.S <88016168+CHITHIRAIKANIS@users.noreply.github.com>
2021-12-19 18:04:52 +0530

    Add files via upload

---
+++ Guvi_React_Assignment_4/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './style.css';
+import ControlledComponent from './DemoForm';
+
+export default function App() {
+  return (
+    <div>
+      <ControlledComponent />
+    </div>
+  );
+}
+
